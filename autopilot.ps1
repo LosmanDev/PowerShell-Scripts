6017bf2602ce5005e005babde37695ef99e550dd
Liban Osman <losmandev@gmail.com>
2026-01-26 20:12:38 -0500

    Create PowerShell script for Windows Autopilot setup
    This script sets up the environment for Windows Autopilot, installs necessary scripts, and extracts hardware IDs to a CSV file.

---
+++ autopilot.ps1
@@ -0,0 +1,30 @@
+Set-ExecutionPolicy Unrestricted
+Install-Script -name Get-WindowsAutopilotInfo -Force
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Get-WindowsAutoPilotInfo.ps1 -online -TenantID "" -Appid "" -appsecret "" -GroupTag 
+
+
+
+<################## MANUAL METHOD ###########################>
+
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+
+# Get the machine’s BIOS serial number
+$serial = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber.Trim()
+
+New-Item -Type Directory -Path "C:\$serial HWID" 
+
+# Change location into the new folder
+Set-Location -Path "C:\$serial HWID"
+
+# Add Scripts folder to your PATH for this session
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+
+# Allow running downloaded scripts in this session
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
+
+# Install and run the Autopilot HWID extractor, outputting inside your serial-named folder
+Install-Script -Name Get-WindowsAutopilotInfo -Force
+Get-WindowsAutopilotInfo -OutputFile "AutopilotHWID.csv"
+
