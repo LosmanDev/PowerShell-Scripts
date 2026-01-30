7355666cfdcdced19c4d73cca025aca1e6f67c89
Liban Osman <losmandev@gmail.com>
2026-01-30 10:32:51 -0500

    Add DisplayName and Home Email to user report

--- ActiveDirectory.ps1
+++ ActiveDirectory.ps1
@@ -10,12 +10,12 @@ $dc.MaxPasswordAge
 $commonParams = @{
     Server     = $dc
     Properties = @(
-        'Enabled', 'Created', 'whenChanged', 'CanonicalName', 'accountExpires', 'AccountExpirationDate',
+        'Enabled', 'Created', 'whenChanged', 'CanonicalName', 'DisplayName', 'accountExpires', 'AccountExpirationDate',
         'pwdLastSet', 'City', 'Department', 'directReports', 'EmployeeID', 'HomeDrive',
         'homePostalAddress', 'LastBadPasswordAttempt', 'LastLogonDate', 'Manager',
         'msDS-cloudExtensionAttribute7', 'msDS-cloudExtensionAttribute6', 'msDS-cloudExtensionAttribute14',
         'Office', 'OfficePhone', 'otherMobile', 'PostalCode', 'proxyAddresses',
-        'SamAccountName', 'State', 'StreetAddress', 'Title', 'UserPrincipalName'
+        'SamAccountName', 'State', 'StreetAddress', 'Title', 'UserPrincipalName', 'msDS-cloudExtensionAttribute3', 'msDS-cloudExtensionAttribute15'
     )
 }
 
@@ -49,19 +49,22 @@ if (-not $user.Enabled) {
         'Legal Hold'     = $user.'msDS-cloudExtensionAttribute7'
         'Manager'        = $user.Manager
         'Title'          = $user.Title 
-        'Department'     = $user.Department 
+        'Department'     = $user.Department
+        'Home Email'     = $user.'msDS-cloudExtensionAttribute3'
     } | Format-List
 }
 else {
     # Enabled account: all the fields
     $report = [ordered]@{
         'Created'              = $user.Created
+        'DisplayName'          = $user.DisplayName
         'User Principal Name'  = $user.UserPrincipalName
         'SamAccountName'       = $user.SamAccountName
         'Employee ID'          = $user.EmployeeID
         'Title'                = $user.Title       
         'Department'           = $user.Department
         'OU Path'              = $user.CanonicalName
+        'Home Email'           = $user.'msDS-cloudExtensionAttribute3'
         'Office'               = $user.Office
         'Office Phone'         = $user.OfficePhone
         'Mobile'               = $user.otherMobile
